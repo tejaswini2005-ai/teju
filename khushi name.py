aa4e945fcb3f54f397ed6e1c29f07a1b38e4ab3d
tejaswini2005-ai <muttupuritejaswini@gmail.com>
2025-08-26 12:08:02 +0530

    Add files via upload

---
+++ khushi name.py
@@ -0,0 +1,20 @@
+def get_personal_details():
+function to read a personals details from the key board
+Name=input("enter your name")
+address=input("enter your address")
+email=input("enter your email id")
+mobile no=("enter your moblile no")
+print("in personal detals in")
+print("name={}in address={}in email={}inmobile no={}")
+format(name,address,email,mobile)
+
+
+
+
+
+
+
+
+
+
+ 
